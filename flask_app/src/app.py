f5ada252158e3fa8e78f2741d01925fa99b32050
Artem Kamyshansky <ka@media5.com>
2024-01-25 21:38:23 +0300

    chore: fix

--- flask_app/src/app.py
+++ flask_app/src/app.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 app.logger = logging.getLogger(__name__)
 app.logger.setLevel(logging.INFO)
 app.logger.addFilter(RequestIdFilter())
-logstash_handler = logstash.LogstashHandler(host='logstash', port=5044, proto='UDP')
+logstash_handler = logstash.LogstashHandler(host='logstash', port=5044, version=1)
 app.logger.addHandler(logstash_handler)
 
 
