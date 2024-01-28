6985c1962171b0b58dfedba8a89012224338b260
Artem Kamyshansky <ka@media5.com>
2024-01-29 00:57:01 +0300

    feat: send logs from nginx, ugc and test flask

--- ugc/src/core/logger.py
+++ ugc/src/core/logger.py
@@ -1,86 +1,73 @@
+import json
 import logging
 from typing import Any
 
 from loguru import logger
 
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+LOG_DEFAULT_HANDLERS = ["console"]
+
 fmt = "{process.id} - {thread.id} - {time} - {name} - {level} - {message}"
 logger.add("server.log", format=fmt, rotation="10MB", compression="zip", enqueue=True)
 
-LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-LOG_DEFAULT_HANDLERS = [
-    "console",
-]
+LOG_FORMAT_JSON = '{"level":"{level}", "message":"{message}", "request_id": 123}'
+
+def serialize(record):
+    subset = {"timestamp": record["time"].timestamp(), "message": record["message"]}
+    return json.dumps(subset)
+
+def formatter(record):
+    record["extra"]["serialized"] = serialize(record)
+    return "{extra[serialized]}\n"
+
+
+logger.add(
+    "/var/log/ugc/rotated.json",
+    level="DEBUG",
+    format=formatter,
+    rotation="10 MB",
+    retention="10 days",
+    compression="zip",
+    serialize=True,
+)
 
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
         "verbose": {"format": LOG_FORMAT},
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "%(levelprefix)s %(message)s",
-            "use_colors": None,
-        },
+        "default": {"()": "uvicorn.logging.DefaultFormatter", "fmt": "%(levelprefix)s %(message)s", "use_colors": None},
         "access": {
             "()": "uvicorn.logging.AccessFormatter",
             "fmt": "%(levelprefix)s %(client_addr)s - '%(request_line)s' %(status_code)s",
         },
     },
     "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-        },
-        "access": {
-            "formatter": "access",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-        },
+        "console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "verbose"},
+        "default": {"formatter": "default", "class": "logging.StreamHandler", "stream": "ext://sys.stdout"},
     },
     "loggers": {
-        "": {
-            "handlers": LOG_DEFAULT_HANDLERS,
-            "level": "INFO",
-        },
-        "uvicorn.error": {
-            "level": "INFO",
-        },
-        "uvicorn.access": {
-            "handlers": ["access"],
-            "level": "INFO",
-            "propagate": False,
-        },
+        "": {"handlers": LOG_DEFAULT_HANDLERS, "level": "INFO"},
+        "uvicorn.error": {"level": "INFO"},
+        # ValueError: Unable to configure logger: "uvicorn.access": {"handlers": ["access"], "level": "INFO", "propagate": False},
     },
-    "root": {
-        "level": "INFO",
-        "formatter": "verbose",
-        "handlers": LOG_DEFAULT_HANDLERS,
-    },
+    "root": {"level": "INFO", "formatter": "verbose", "handlers": LOG_DEFAULT_HANDLERS},
 }
 
 
 class LoggerAdapter:
-    """
-    A LoggerAdapter adapts a `loguru` logger to be compatible with the standard `logging` library.
-    """
+    """A LoggerAdapter adapts a `loguru` logger to be compatible with the standard `logging` library."""
 
     def __init__(self, logger: "logger"):
-        """
-        Initialize a LoggerAdapter.
+        """Initialize a LoggerAdapter.
 
         :param logger: The logger object from `loguru` to be adapted.
         """
         self.logger = logger
 
     def log(self, level: int, message: str, *args, **kwargs):
-        """
-        Logs a message with the specified severity.
+        """Logs a message with the specified severity.
 
         :param level: The severity level of the log.
         :param message: The message to be logged.
@@ -99,8 +86,7 @@ class LoggerAdapter:
             self.logger.debug(message, **kwargs)
 
     def __getattr__(self, name: str) -> Any:
-        """
-        Retrieves an attribute from the logger object.
+        """Retrieves an attribute from the logger object.
 
         :param name: Name of the attribute.
         :returns: The attribute value.
